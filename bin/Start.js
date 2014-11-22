e80da3522feee248f9e916b3f91793042542465d
bousfoul <bousfoul@gmail.com>
2014-11-22 20:03:45 +0100

    First Commit

---
+++ bin/Start.js
@@ -0,0 +1,8 @@
+// Load application module
+var app = require('../lib/app');
+// Start server
+var server = app.listen(1337, 'localhost', function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Server running at http://%s:%s', host, port);
+});
